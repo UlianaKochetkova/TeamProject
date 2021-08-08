dbbdafc55ec407ed2638d6e0bc78ca3924922049
Maxim <ilimar2781@mail.ru>
2021-08-08 16:34:07 +0300

    With AJAX

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -5,42 +5,46 @@ $('#action_menu_btn').click(function(){
 	});
 
 /////////////////////////////////////////////////////////////////КОД ДАШИ
-function someFunc(){
+function someFunc(tagid){
 	//console.log('Функция вызвана');
 	//https://learn.javascript.ru/xmlhttprequest
-//XMLHttpRequest - встроенный в браузер объект, который даёт возможность делать HTTP-запросы к серверу без перезагрузки страницы
+	//XMLHttpRequest - встроенный в браузер объект, который даёт возможность делать HTTP-запросы к серверу без перезагрузки страницы
 	var xhr = new XMLHttpRequest();
-	let tagid=1;
-//Инициализация. Только конфигурирует запрос
-	xhr.open('GET', 'jsp/servletPage.jsp',true);
+	//Инициализация. Только конфигурирует запрос
+	//Так как GET запрос, параметры записаны в url
+	xhr.open("GET", "/getTagMsgs?tagid="+tagid, true);
+	
+	//Устанавливает заголовок запроса с именем name и значением value
+	//value - определяет правильную кодировку для переменных POST. https://en.wikipedia.org/wiki/POST_(HTTP)#Use_for_submitting_web_forms
+	//xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	
+	
+	//метод устанавливает соединение и отсылает запрос к серверу
+	//encodeURI - кодирует строку
+	//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
+	//если вы хотите отправить что-то в качестве параметра сервлету, используйте метод send, иначе send null
+	//параметр запроса на бэк
+	xhr.send();
 
-//Устанавливает заголовок запроса с именем name и значением value
-//value - определяет правильную кодировку для переменных POST. https://en.wikipedia.org/wiki/POST_(HTTP)#Use_for_submitting_web_forms
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 //Функция для проверки какого-либо ответа
 // 200 = OK
 	xhr.onload = function() {
-		console.log('Функция вызвана');
+		console.log(`Загружено: ${xhr.status} ${xhr.response}`);
 		if (xhr.status === 200) {
 			//TODO:Что здесь за действие?
 			//Обработка того, что пришло с бэка
 			//jQuery эквивалетно - success
 			//doSomeAction(xhr.responseText, param1);
 			//success(xhr.responseText);
-			// console.log(xhr.responseText);
-			return false;
+			document.getElementById("msgs_body").innerHTML = xhr.response;
 		} else {
 			alert('Request failed. Returned status of ' + xhr.status);
 		}
 	};
-
-//метод устанавливает соединение и отсылает запрос к серверу
-//encodeURI - кодирует строку
-//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
-//если вы хотите отправить что-то в качестве параметра сервлету, используйте метод send, иначе send null
-//параметр запроса на бэк
-	xhr.send(encodeURI('tagid=' + tagid));
+	
+	
+	
 }
 
 //Функция, которая обрабатывает то, что пришло с бэка
